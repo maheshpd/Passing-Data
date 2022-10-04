2f78ee2ca92f3f5590f5ecb5908e95f5491d984e
MaheshI2T <mahesh.prasad@ideas2tech.com>
2022-10-04 11:01:14 +0530

    Initial Commit

---
+++ PassingData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassingData
+//
+//  Created by Mahesh Prasad on 04/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
